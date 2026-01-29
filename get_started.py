ce039f7c8c3ea68e2d1e1401e0c0d8254d99d5ce
Alibektiger <al_aibekuly@kbtu.kz>
2026-01-29 23:24:16 +0500

    gget_started.py

---
+++ get_started.py
@@ -0,0 +1,12 @@
+#1
+import sys
+print(sys.version)
+#2
+#What is the correct file extension for Python files?
+#.py
+#3
+#What is a correct command line syntax for checking if python is installed on your computer? (And also to check the Python version)
+#python --version
+#4
+# What is a correct syntax to exit the Python command line interface?
+# exit()
