a154de2faf1bcb009b851d94c0a19f5c139a8d6e
Alibektiger <al_aibekuly@kbtu.kz>
2026-01-29 23:39:16 +0500

    syntax.py

---
+++ syntax.py
@@ -0,0 +1,23 @@
+#1
+if 6 > 2:
+  print("Six is greater than two!")
+  
+#2
+#if 6 > 2:
+#print("Six is greater than two!") 
+#!!! Syntax Error:  Python will give you an error if you skip the indentation:
+
+#3
+x = 8
+y = "number is eight"
+
+print(x)
+print(y)
+
+#4
+#This is a comment.For comment we use #
+print("Hello, World!")
+
+#5
+
+print("Learning Python is fun!")
